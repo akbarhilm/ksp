331dafeabb7875bfb304469d371fd3f6c912e52d
akbarhilm <akbarhilm@gmail.com>
2025-11-13 21:17:09 +0700

    footer signature belum

--- resources/views/pdf/sphutang.blade.php
+++ resources/views/pdf/sphutang.blade.php
@@ -1,18 +1,192 @@
 <x-layout bodyClass="g-sidenav-show  bg-gray-200">
     <main>
-      <div class="flex justify-center items-start min-h-screen bg-gray-200 p-4">
-    <div class="w-full max-w-[210mm] h-[297mm] bg-white shadow-lg font-sans ">
+      <div class="d-flex justify-content-center items-middle min-h-screen bg-gray-200 p-4">
+    <div class=" bg-white shadow-lg font-sans " style="max-width:210mm; height:auto">
         <!-- A4 content goes here -->
-        {{-- <div class="text-center mb-6 py-6">
-            <h4 class="text-xl font-bold text-decoration-underline">SURAT PERJANJIAN HUTANG PIUTANG</h4>
-            <span class="text-md">Nomor Anggota : 2024</span>
+         <div class="text-center mb-2 mt-6">
+            <h6 class="text-xl font-bold text-decoration-underline">SURAT PERJANJIAN HUTANG PIUTANG</h6>
+            <span class="text-xs">Nomor Anggota : 2024</span>
         </div>
-        <div class="px-10">
-            <span>Yang bertanda tangan di bawah ini:</span>
-        </div> --}}
-    
-        {{-- <div class="px-11">
-            <div class="mt-3" style="line-height: 2;">
+        <div class="px-5">
+            <span class="text-sm">Yang bertanda tangan di bawah ini:</span>
+        </div> 
+        <div class=" px-6">
+        <table class="table table-borderless w-100 text-sm">
+            <tr>
+                <td>1.</td>
+                <td>Nama</td>
+                <td>:</td>
+                <td>MOCHAMAD MEGANTARA</td>
+</tr>
+<tr>
+    <td></td>
+    <td>Alamat</td>
+    <td>:</td>
+    <td> KAV SETIA BUDI RT 002/005 DESA CIPADU LARANGAN – TANGERANG </td>
+</tr>
+<tr>
+    <td></td>
+    <td>NIK</td>
+    <td>:</td>
+    <td>3671132305040002</td>
+</tr>
+<tr>
+    <td></td>
+    <td>Pekerjaan</td>
+    <td>:</td>
+    <td> PT VIRTUS FACILITY SERVICE</td>
+</tr>
+<tr>
+    <td></td>
+    <td>No HP</td>
+    <td>:</td>
+    <td>083870411531</td>
+</tr>
+<tr>
+    <td></td>
+    <td colspan="3">Selanjutnya disebut PIHAK KESATU atau YANG BERHUTANG</td>
+</tr>
+<tr>
+    <td></td>
+    <td colspan="3"><p class="text-wrap text-sm" style="text-align:justify; text-justify:inner-word">Kepala Cabang Koperasi KSP SINAR MURNI- SEJAHTERA PASAR KEMIS  dalam hal tersebut 
+dan atas nama Koperasi “SINAR MURNI  SEJAHTERA” Beralamat DIJALAN RAYA BUMI INDAH 
+BLOK RYER  PASAR KEMIS-TANGERANG, Selanjutnya disebut PIHAK KEDUA atau KOPERASI.</p></td>
+</tr>
+
+</table>
+<div class="text-center mb-4">
+            <span class="text-decoration-underline text-sm">KEDUA BELAH PIHAK MENGADAKAN PERJANJIAN SEBAGAI BERIKUT</span>
+</div>
+<table class="table table-borderless  w-100 text-sm">
+            <tr>
+                <td>a.</td>
+                <td><p class="text-wrap text-sm" style="text-align:justify; text-justify:inner-word">PIHAK KESATU mengaku telah berhutang dari PIHAK KEDUA sebesar <b>6.000.000 (Enam  
+Juta Rupiah)</b></p></td>
+                
+</tr>
+<tr>
+    <td>b.</td>
+    <td><p class="text-wrap text-sm" style="text-align:justify; text-justify:inner-word">Untuk pengembalian pinjaman Tersebut <b>PIHAK KESATU</b> bersedia diambil angsurannya 
+melalui ATM oleh <b>PIHAK KEDUA</b> sesuai dengan angsuran pinjaman yang telah ditetapkan & 
+disepakati kedua belah pihak sebesar Rp <b>1.190.000 (Satu Juta Seratus Sembilan Puluh 
+Ribu  Rupiah</b></p></td>
+</tr>
+<tr>
+    <td>c.</td>
+    <td><p class="text-wrap text-sm" style="text-align:justify; text-justify:inner-word">Setiap bulan selama 6 kali angsuran mulai bulan Desember 2025 sampai dengan bulan Mei 
+2026 atau sampai hutang PIHAK KESATU lunas. Dengan ketentuan apabila terjadi 
+keterlambatan pembayaran maka setiap bulannya akan dikenakan denda sebesar 1% 
+perbulan atau 0.1% perhari dari saldo pinjaman.</p></td>
+</tr>
+<tr>
+    <td>d.</td>
+    <td><p class="text-wrap text-sm" style="text-align:justify; text-justify:inner-word">PIHAK KESATU menyetujui potongan simpanan Rp. 120.000 dan biaya tata 
+laksana/administrasi Rp. 180.000 Atas dasar pinjaman tersebut, saya PIHAK KESATU dengan 
+sukarela menitip dengan PIHAK KEDUA berupa :</p></td>
+</table>
+<div class=" px-5 my-n4">
+<table class="table table-borderless w-100 text-xs">
+    <tr>
+        <td class="text-xs">1.</td>
+        <td class="text-xs">ATM</td>
+        <td class="text-xs">BRI 5221 8402 6684 2641</td>
+</tr>
+<tr>
+        <td class="text-xs">2.</td>
+        <td class="text-xs">PIN ATM</td>
+        <td class="text-xs">230 516</td>
+</tr>
+<tr>
+        <td class="text-xs">3.</td>
+        <td class="text-xs">JAMSOSTEK</td>
+        <td class="text-xs">23045037407</td>
+</tr>
+<tr>
+        <td class="text-xs">4.</td>
+        <td class="text-xs">BUKU TABUNGAN</td>
+        <td class="text-xs">0261-01-135095-50-0/td>
+</tr>
+<tr>
+        <td class="text-xs">5.</td>
+        <td class="text-xs">IJAZAH SMP</td>
+        <td class="text-xs">a/n Mochammad Megantara DN-30/D-SMP/06/0126240</td>
+</tr>
+<tr>
+        <td class="text-xs">6.</td>
+        <td class="text-xs">IJAZAH SMK</td>
+        <td class="text-xs">a/n Moch Edwin Apriza DN-MK/06 0030173</td>
+</tr>
+<tr>
+        <td class="text-xs">7.</td>
+        <td class="text-xs">AKTA KELAHIRAN</td>
+        <td class="text-xs">a/n Mochammad Megantara CSL 8330/2005</td>
+</tr>
+<tr>
+        <td class="text-xs">8.</td>
+        <td class="text-xs">KARTU KELUARGA</td>
+        <td class="text-xs">3671130212140006</td>
+</tr>
+
+</table>
+</div>
+<table class="table table-borderless text-sm">
+</tr>
+<td></td>
+<td>
+<p class="text-wrap text-sm " style="text-align:justify; text-justify:inner-word"> Saya tidak keberatan gaji saya diambil melalui ATM dan sisanya saya ambil dari KSP SINAR 
+MURNI – SEJAHTERA PASAR KEMIS tunai/ non tunai dipotong cicilan pinjaman perbulan 
+sampai pinjaman saya lunas . </p></td>
+</tr>
+
+    <tr>
+        <td>e.</td>
+        <td><p class="text-wrap text-sm" style="text-align:justify; text-justify:inner-word">Untuk angsuran pada point B, PIHAK KESATU menyetujui diamBil gaji pertama setelah 
+perjanjian akad kredit ini di buat.</p></td>
+</tr>
+<tr>
+        <td>f.</td>
+        <td><p class="text-wrap text-sm" style="text-align:justify; text-justify:inner-word">Bilamana PIHAK KESATU tidak bekerja lagi atau di PHK dari pekerjaannya, maka PIHAK KESATU 
+harus melunasi sisa utangnya sesuai dengan perjanjian Kredit.</p></td>
+</tr>
+<tr>
+        <td>g.</td>
+        <td><p class="text-wrap text-sm" style="text-align:justify; text-justify:inner-word">Apabila PIHAK KESATU melanggar Surat Perjanjian ini, maka bersedia dituntut di Pengadilan 
+Negeri Kab. Tangerang, dengan catatan seluruh biaya perkara ditanggung oleh PIHAK KESATU</p></td>
+</tr>
+<tr>
+        <td>h.</td>
+        <td><p class="text-wrap text-sm" style="text-align:justify; text-justify:inner-word">Dan apabila ada pinjaman baik dari BANK atau Pihak Lain yang masuk ke rekening yang 
+saya jaminkan kepada KSP SINAR MURNI, maka saya siap melunasi hutang di KOPERASI 
+SIMPAN PINJAM SINAR MURNI Secara Flat tidak ada pengurangan pokok maupun bunga.</p></td>
+</tr>
+<tr>
+        <td>i.</td>
+        <td><p class="text-wrap text-sm" style="text-align:justify; text-justify:inner-word">PIHAK KESATU berjanji tidak akan mengambil BERKAS diatas yang saya titipkan sebelum pinjaman 
+saya lunas dan tidak akan merekayasa system pengambilan gaji seperti merubah/memblokir 
+No.Rekening, No.Pin ataupun MOBILE BANKING, SMS BANKING dan INTERNET BANKING  yang 
+menyebabkan proses pembayaran angsuran ke PIHAK KEDUA terhambat.</p></td>
+</tr>
+<tr>
+        <td>j.</td>
+        <td><p class="text-wrap text-sm" style="text-align:justify; text-justify:inner-word"> Bilamana PIHAK KESATU tidak bekerja lagi atau PHK (PEMUTUSAN HUBUNGAN KERJA) dari 
+pekerjaannya, maka PIHAK KESATU harus melunasi sisa utangnya dari pesangon dan SALDO 
+JAMSOSTEK ataupun dari hal lain dengan jumlah sisa hutangnya yang tertera di perjanjian 
+kredit.</p></td>
+</tr>
+<tr>
+        <td>k.</td>
+        <td><p class="text-wrap text-sm" style="text-align:justify; text-justify:inner-word"> Apabila PIHAK KESATU melanggar Surat Perjanjian ini, maka bersedia dituntut di Pengadilan 
+Negeri Tangerang, dengan catatan seluruh biaya perkara ditanggung oleh PIHAK KESATU.</p></td>
+</tr>
+</table>
+</div>
+<div class="px-5 my-n3">
+    <p class="text-wrap text-sm" style="text-align:justify; text-justify:inner-word">Demikian Surat Perjanjian ini dibuat dengan pikiran sehat dan tenang tanpa ada paksaan dari pihak 
+lain , coretan dan penambahan dianggap sah oleh kedua belah pihak , lalu ditanda tangani masing
+masing pihak dengan diberi materai secukupnya untuk dijadikan bukti yang sah</p>
+            
+         <!-- <div class="px-11"> -->
+            <!-- <div class="mt-3" style="line-height: 2;">
                 <span>1.</span> <span class="px-2">Nama : ....................................................</span>
                 <br>
                 <span class="px-4">Alamat : ..................................................</span>
@@ -30,9 +204,9 @@
                <p class="px-4 py-2" style="text-align: justify;
   text-justify: inter-word;">Kepala Cabang Koperasi KSP SINAR MURNI- SEJAHTERA PASAR KEMIS  dalam hal tersebut dan atas nama Koperasi “SINAR MURNI  SEJAHTERA” Beralamat DIJALAN RAYA BUMI INDAH 
 BLOK RYER  PASAR KEMIS-TANGERANG, Selanjutnya disebut PIHAK KEDUA atau KOPERASI.</p>
+</div> -->
             
-            </div>
-            <div class="text-center mb-2 ">
+            <!-- <div class="text-center mb-2 ">
             <span class="  text-decoration-underline">  KEDUA BELAH PIHAK MENGADAKAN PERJANJIAN SEBAGAI BERIKUT</span>
             
         </div>
@@ -47,29 +221,8 @@ Juta Rupiah)</b></span>
 melalui ATM oleh <b>PIHAK KEDUA</b> sesuai dengan angsuran pinjaman yang telah ditetapkan & 
 disepakati kedua belah pihak sebesar Rp <b>1.190.000 (Satu Juta Seratus Sembilan Puluh 
 Ribu  Rupiah)</b></p></div>
-    </div>
-</div> --}}
-<p style="text-align: center;"><span style="text-decoration: underline;"><strong>SURAT PERJANJIAN HUTANG PIUTANG</strong></span></p>
-<p style="text-align: center;">Nomor anggota : 10212</p>
-<p style="text-align: left;">&nbsp;</p>
-<p style="text-align: left;">Yang bertanda tangan dibawah ini :</p>
-<ol>
-<li style="text-align: left;">&nbsp; &nbsp;Nama : ......................</li>
-</ol>
-<p style="padding-left: 40px;">&nbsp; &nbsp;Alamat : ..............................</p>
-<p style="padding-left: 40px;">&nbsp;</p>
-<p style="padding-left: 40px;">&nbsp; &nbsp;NIK : ..................................</p>
-<p style="padding-left: 40px;">&nbsp; &nbsp;Pekerjaan : .............................</p>
-<p style="padding-left: 40px;">&nbsp; &nbsp;No HP : ...................................</p>
-<p>Selanjutnya disebut PIHAK KESATU atau YANG BERHUTANG</p>
-<p style="text-align: justify;">Kepala Cabang Koperasi KSP SINAR MURNI- SEJAHTERA PASAR KEMIS dalam hal tersebut <br />dan atas nama Koperasi &ldquo;SINAR MURNI SEJAHTERA&rdquo; Beralamat DIJALAN RAYA BUMI INDAH <br />BLOK RYER PASAR KEMIS-TANGERANG, Selanjutnya disebut PIHAK KEDUA atau KOPERASI.</p>
-<p style="text-align: center;"><span style="text-decoration: underline;"> KEDUA BELAH PIHAK MENGADAKAN PERJANJIAN SEBAGAI BERIKUT</span></p>
-<ol style="list-style-type: lower-alpha;">
-<li style="text-align: justify;">PIHAK KESATU mengaku telah berhutang dari PIHAK KEDUA sebesar <strong>6.000.000 (Enam </strong><br /><strong>Juta Rupiah)</strong></li>
-<li style="text-align: justify;">Untuk pengembalian pinjaman Tersebut <strong>PIHAK KESATU</strong> bersedia diambil angsurannya <br />melalui ATM oleh <strong>PIHAK KEDUA</strong> sesuai dengan angsuran pinjaman yang telah ditetapkan &amp; <br />disepakati kedua belah pihak sebesar Rp <strong>1.190.000 (Satu Juta Seratus Sembilan Puluh </strong><br /><strong>Ribu Rupiah)</strong></li>
-<li style="text-align: justify;">Setiap bulan selama 6 kali angsuran mulai bulan Desember 2025 sampai dengan bulan Mei <br />2026 atau sampai hutang PIHAK KESATU lunas. Dengan ketentuan apabila terjadi <br />keterlambatan pembayaran maka setiap bulannya akan dikenakan denda sebesar 1% <br />perbulan atau 0.1% perhari dari saldo pinjaman.</li>
-<li style="text-align: justify;">PIHAK KESATU menyetujui potongan simpanan Rp. 120.000 dan biaya tata <br />laksana/administrasi Rp. 180.000 Atas dasar pinjaman tersebut, saya PIHAK KESATU dengan <br />sukarela menitip dengan PIHAK KEDUA berupa :</li>
-</ol>
+    </div> -->
+<!-- </div> -->
     </div>
       </div>
     </main>
