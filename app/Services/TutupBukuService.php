b8f402c937c060e81a08c976101aeee00182735d
akbarhilm <akbarhilm@gmail.com>
2026-02-16 16:24:43 +0700

    tutup buku bulana tidak pindah buku

--- app/Services/TutupBukuService.php
+++ app/Services/TutupBukuService.php
@@ -128,61 +128,61 @@ $month = substr($lt,5,2);
                     'id_entry' =>  0,
                 ]);
             }
-          $akunSHU = Akun::where('id_akun','43')->first();
-$akunPend = Akun::where('tipe_akun','Pendapatan')->get();
-$akunBeban = Akun::where('tipe_akun','Beban')->get();
+//           $akunSHU = Akun::where('id_akun','43')->first();
+// $akunPend = Akun::where('tipe_akun','Pendapatan')->get();
+// $akunBeban = Akun::where('tipe_akun','Beban')->get();
 
 
-            foreach($akunPend as $p){
-    $saldoPendapatan = Jurnal::where('id_akun',$p->id_akun)->whereYear('tanggal_transaksi',$year)->whereMonth('tanggal_transaksi',$month)->sum(DB::raw('v_kredit - v_debet'));
+//             foreach($akunPend as $p){
+//     $saldoPendapatan = Jurnal::where('id_akun',$p->id_akun)->whereYear('tanggal_transaksi',$year)->whereMonth('tanggal_transaksi',$month)->sum(DB::raw('v_kredit - v_debet'));
 
-    if($saldoPendapatan > 0){
-        Jurnal::create([
-            'tanggal_transaksi' => $tanggal,
-            'id_akun' => $p->id_akun,
-            'no_jurnal'=>$nojurnal,
-            'v_debet' => $saldoPendapatan,
-            'v_kredit' => 0,
-            'keterangan' => 'Tutup Buku Pendapatan',
-             'id_entry' => 0
-        ]);
+//     if($saldoPendapatan > 0){
+//         Jurnal::create([
+//             'tanggal_transaksi' => $tanggal,
+//             'id_akun' => $p->id_akun,
+//             'no_jurnal'=>$nojurnal,
+//             'v_debet' => $saldoPendapatan,
+//             'v_kredit' => 0,
+//             'keterangan' => 'Tutup Buku Pendapatan',
+//              'id_entry' => 0
+//         ]);
 
-        Jurnal::create([
-            'tanggal_transaksi' => $tanggal,
-            'id_akun' => $akunSHU->id_akun,
-            'no_jurnal'=>$nojurnal,
-            'v_debet' => 0,
-            'v_kredit' => $saldoPendapatan,
-            'keterangan' => 'SHU dari Pendapatan',
-             'id_entry' => 0
-        ]);
-    }
-}
-    foreach($akunBeban as $b){
-    $saldoBeban = Jurnal::where('id_akun',$b->id_akun)->whereYear('tanggal_transaksi',$year)->whereMonth('tanggal_transaksi',$month)->sum(DB::raw('v_debet - v_kredit'));
+//         Jurnal::create([
+//             'tanggal_transaksi' => $tanggal,
+//             'id_akun' => $akunSHU->id_akun,
+//             'no_jurnal'=>$nojurnal,
+//             'v_debet' => 0,
+//             'v_kredit' => $saldoPendapatan,
+//             'keterangan' => 'SHU dari Pendapatan',
+//              'id_entry' => 0
+//         ]);
+//     }
+// }
+//     foreach($akunBeban as $b){
+//     $saldoBeban = Jurnal::where('id_akun',$b->id_akun)->whereYear('tanggal_transaksi',$year)->whereMonth('tanggal_transaksi',$month)->sum(DB::raw('v_debet - v_kredit'));
 
-    if($saldoBeban > 0){
-        Jurnal::create([
-            'tanggal_transaksi' => $tanggal,
-            'id_akun' => $akunSHU->id_akun,
-            'no_jurnal'=>$nojurnal,
-            'v_debet' => $saldoBeban,
-            'v_kredit' => 0,
-            'keterangan' => 'SHU untuk Beban',
-             'id_entry' => 0
-        ]);
+//     if($saldoBeban > 0){
+//         Jurnal::create([
+//             'tanggal_transaksi' => $tanggal,
+//             'id_akun' => $akunSHU->id_akun,
+//             'no_jurnal'=>$nojurnal,
+//             'v_debet' => $saldoBeban,
+//             'v_kredit' => 0,
+//             'keterangan' => 'SHU untuk Beban',
+//              'id_entry' => 0
+//         ]);
 
-        Jurnal::create([
-            'tanggal_transaksi' => $tanggal,
-            'id_akun' => $b->id_akun,
-            'no_jurnal'=>$nojurnal,
-            'v_debet' => 0,
-            'v_kredit' => $saldoBeban,
-            'keterangan' => 'Tutup Buku Beban',
-             'id_entry' => 0
-        ]);
-    }
-}
+//         Jurnal::create([
+//             'tanggal_transaksi' => $tanggal,
+//             'id_akun' => $b->id_akun,
+//             'no_jurnal'=>$nojurnal,
+//             'v_debet' => 0,
+//             'v_kredit' => $saldoBeban,
+//             'keterangan' => 'Tutup Buku Beban',
+//              'id_entry' => 0
+//         ]);
+//     }
+// }
 
             TutupBuku::create(['tanggal' => $tanggal]);
 
